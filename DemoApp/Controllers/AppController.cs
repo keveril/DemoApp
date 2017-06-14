f79cb808ed58d19d4478b0aded432af8b795b8d9
Sailaxman <skumar@spscommerce.com.au>
2017-06-14 10:11:32 +1000

    DemoApp
    v1.0

---
+++ DemoApp/Controllers/AppController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DemoApp.ViewModels;
+
+namespace DemoApp.Controllers.Web
+{
+    public class AppController : Controller
+    {
+      
+
+        public AppController()
+        {
+          
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+       
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ DemoApp/wwwroot/js/site.js
@@ -0,0 +1,32 @@
+﻿// Write your Javascript code.
+(function () {
+
+
+
+    var ele = $("#username");
+    ele.text("sailaxman kumar");
+
+    var main = $("#main");
+    main.on("mouseenter", function () {
+        main.style = "background-color: #888;";
+    });
+
+    main.onmouseleave = function () {
+        main.style.backgroundColor = "";
+    };
+
+
+    var $sidebarAndWrapper = $("#sidebar,#wrapper");
+    var $icon = $("#sidebarToggle i.fa")
+    $("#sidebarToggle").on("click", function () {
+        $sidebarAndWrapper.toggleClass("hide-sidebar");
+        if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
+            $icon.removeClass("fa-angle-left");
+            $icon.addClass("fa-angle-right");
+        }
+        else {
+            $icon.removeClass("fa-angle-right");
+            $icon.addClass("fa-angle-left");
+        }
+    });
+})();
